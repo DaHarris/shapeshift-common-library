6997d5dc13dce8a883fa68b4172a8c74e57c422c
DaHarris <dharrisdnv@gmail.com>
2017-06-01 14:47:57 -0600

    add exchange/ticker schema

---
+++ models/Exchange.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const TickerSchema = new mongoose.Schema({
+  symbol: {type: String, required: true, unique: true},
+  exchangeSymbol: {type: String, required: true, unique: true},
+  high: Number,
+  low: Number,
+  last: Number,
+  bid: Number,
+  ask: Number
+})
+
+const ExchangeSchema = new mongoose.Schema({
+  exchangeID: {type: String, required: true, unique: true},
+  exchangeName: {type: String, required: true, unique: true},
+  tickers: [TickerSchema]
+})
+
+module.exports = {
+  model: mongoose.model('Exchange', ExchangeSchema)
+}
